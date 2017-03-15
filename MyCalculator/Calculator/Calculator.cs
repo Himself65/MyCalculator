ae6d966444fc6e2c548cc9f82e1334fd0c9d20b0
扩散性百万甜面包 <761282619@qq.com>
2017-03-15 13:40:44 +0800

    类库修改

---
+++ MyCalculator/Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClass
+{
+   public class Calculator
+    {
+        public double a, b;
+        public double Add()
+        {
+            return this.a + this.b;
+        }
+        public double Sub()
+        {
+            return this.a * this.b;
+        } 
+        public double Max()
+        {
+            if (this.a > this.b)
+                return a;
+            else
+                return b;
+        }
+        public double Min()
+        {
+            if (this.a < this.b)
+                return a;
+            else
+                return b;
+        }
+
+    }
+}
