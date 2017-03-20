52f75983228854706feef7703f079f409faaf393
扩散性百万甜面包 <761282619@qq.com>
2017-03-20 23:35:02 +0800

    增加项目

---
+++ MyCalculator/textForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace MyCalculator
+{
+	partial class TextForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.NumberZero = new System.Windows.Forms.Button();
+			this.NumberOne = new System.Windows.Forms.Button();
+			this.NumberTwo = new System.Windows.Forms.Button();
+			this.NumberThree = new System.Windows.Forms.Button();
+			this.NumberSix = new System.Windows.Forms.Button();
+			this.NumberFive = new System.Windows.Forms.Button();
+			this.NumberFour = new System.Windows.Forms.Button();
+			this.NumberNine = new System.Windows.Forms.Button();
+			this.NumberEight = new System.Windows.Forms.Button();
+			this.NumberSeven = new System.Windows.Forms.Button();
+			this.AddBotton = new System.Windows.Forms.Button();
+			this.MulButton = new System.Windows.Forms.Button();
+			this.ExceptButton = new System.Windows.Forms.Button();
+			this.LessBotton = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// NumberZero
+			// 
+			this.NumberZero.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberZero.AutoSize = true;
+			this.NumberZero.Location = new System.Drawing.Point(174, 214);
+			this.NumberZero.Name = "NumberZero";
+			this.NumberZero.Size = new System.Drawing.Size(48, 45);
+			this.NumberZero.TabIndex = 7;
+			this.NumberZero.Text = "0";
+			this.NumberZero.UseVisualStyleBackColor = true;
+			// 
+			// NumberOne
+			// 
+			this.NumberOne.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberOne.AutoSize = true;
+			this.NumberOne.Location = new System.Drawing.Point(12, 112);
+			this.NumberOne.Name = "NumberOne";
+			this.NumberOne.Size = new System.Drawing.Size(48, 45);
+			this.NumberOne.TabIndex = 0;
+			this.NumberOne.Text = "1";
+			this.NumberOne.UseVisualStyleBackColor = true;
+			// 
+			// NumberTwo
+			// 
+			this.NumberTwo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberTwo.AutoSize = true;
+			this.NumberTwo.Location = new System.Drawing.Point(66, 112);
+			this.NumberTwo.Name = "NumberTwo";
+			this.NumberTwo.Size = new System.Drawing.Size(48, 45);
+			this.NumberTwo.TabIndex = 1;
+			this.NumberTwo.Text = "2";
+			this.NumberTwo.UseVisualStyleBackColor = true;
+			// 
+			// NumberThree
+			// 
+			this.NumberThree.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberThree.AutoSize = true;
+			this.NumberThree.Location = new System.Drawing.Point(120, 112);
+			this.NumberThree.Name = "NumberThree";
+			this.NumberThree.Size = new System.Drawing.Size(48, 45);
+			this.NumberThree.TabIndex = 2;
+			this.NumberThree.Text = "3";
+			this.NumberThree.UseVisualStyleBackColor = true;
+			// 
+			// NumberSix
+			// 
+			this.NumberSix.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberSix.AutoSize = true;
+			this.NumberSix.Location = new System.Drawing.Point(120, 163);
+			this.NumberSix.Name = "NumberSix";
+			this.NumberSix.Size = new System.Drawing.Size(48, 45);
+			this.NumberSix.TabIndex = 3;
+			this.NumberSix.Text = "6";
+			this.NumberSix.UseVisualStyleBackColor = true;
+			// 
+			// NumberFive
+			// 
+			this.NumberFive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberFive.AutoSize = true;
+			this.NumberFive.Location = new System.Drawing.Point(66, 163);
+			this.NumberFive.Name = "NumberFive";
+			this.NumberFive.Size = new System.Drawing.Size(48, 45);
+			this.NumberFive.TabIndex = 4;
+			this.NumberFive.Text = "5";
+			this.NumberFive.UseVisualStyleBackColor = true;
+			// 
+			// NumberFour
+			// 
+			this.NumberFour.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberFour.AutoSize = true;
+			this.NumberFour.Location = new System.Drawing.Point(12, 163);
+			this.NumberFour.Name = "NumberFour";
+			this.NumberFour.Size = new System.Drawing.Size(48, 45);
+			this.NumberFour.TabIndex = 5;
+			this.NumberFour.Text = "4";
+			this.NumberFour.UseVisualStyleBackColor = true;
+			// 
+			// NumberNine
+			// 
+			this.NumberNine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberNine.AutoSize = true;
+			this.NumberNine.Location = new System.Drawing.Point(120, 214);
+			this.NumberNine.Name = "NumberNine";
+			this.NumberNine.Size = new System.Drawing.Size(48, 45);
+			this.NumberNine.TabIndex = 6;
+			this.NumberNine.Text = "9";
+			this.NumberNine.UseVisualStyleBackColor = true;
+			// 
+			// NumberEight
+			// 
+			this.NumberEight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberEight.AutoSize = true;
+			this.NumberEight.Location = new System.Drawing.Point(66, 214);
+			this.NumberEight.Name = "NumberEight";
+			this.NumberEight.Size = new System.Drawing.Size(48, 45);
+			this.NumberEight.TabIndex = 8;
+			this.NumberEight.Text = "8";
+			this.NumberEight.UseVisualStyleBackColor = true;
+			// 
+			// NumberSeven
+			// 
+			this.NumberSeven.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.NumberSeven.AutoSize = true;
+			this.NumberSeven.Location = new System.Drawing.Point(12, 214);
+			this.NumberSeven.Name = "NumberSeven";
+			this.NumberSeven.Size = new System.Drawing.Size(48, 45);
+			this.NumberSeven.TabIndex = 9;
+			this.NumberSeven.Text = "7";
+			this.NumberSeven.UseVisualStyleBackColor = true;
+			// 
+			// AddBotton
+			// 
+			this.AddBotton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.AddBotton.AutoSize = true;
+			this.AddBotton.Location = new System.Drawing.Point(174, 112);
+			this.AddBotton.Name = "AddBotton";
+			this.AddBotton.Size = new System.Drawing.Size(48, 45);
+			this.AddBotton.TabIndex = 10;
+			this.AddBotton.Text = "+";
+			this.AddBotton.UseVisualStyleBackColor = true;
+			// 
+			// MulButton
+			// 
+			this.MulButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.MulButton.AutoSize = true;
+			this.MulButton.Location = new System.Drawing.Point(174, 163);
+			this.MulButton.Name = "MulButton";
+			this.MulButton.Size = new System.Drawing.Size(48, 45);
+			this.MulButton.TabIndex = 11;
+			this.MulButton.Text = "*";
+			this.MulButton.UseVisualStyleBackColor = true;
+			// 
+			// ExceptButton
+			// 
+			this.ExceptButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.ExceptButton.AutoSize = true;
+			this.ExceptButton.Location = new System.Drawing.Point(228, 163);
+			this.ExceptButton.Name = "ExceptButton";
+			this.ExceptButton.Size = new System.Drawing.Size(48, 45);
+			this.ExceptButton.TabIndex = 12;
+			this.ExceptButton.Text = "/";
+			this.ExceptButton.UseVisualStyleBackColor = true;
+			// 
+			// LessBotton
+			// 
+			this.LessBotton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.LessBotton.AutoSize = true;
+			this.LessBotton.Location = new System.Drawing.Point(228, 112);
+			this.LessBotton.Name = "LessBotton";
+			this.LessBotton.Size = new System.Drawing.Size(48, 45);
+			this.LessBotton.TabIndex = 13;
+			this.LessBotton.Text = "-";
+			this.LessBotton.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button4.AutoSize = true;
+			this.button4.Location = new System.Drawing.Point(228, 214);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(48, 45);
+			this.button4.TabIndex = 14;
+			this.button4.Text = "=";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(262, 38);
+			this.richTextBox1.TabIndex = 15;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// TextForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(297, 271);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.LessBotton);
+			this.Controls.Add(this.ExceptButton);
+			this.Controls.Add(this.MulButton);
+			this.Controls.Add(this.AddBotton);
+			this.Controls.Add(this.NumberSeven);
+			this.Controls.Add(this.NumberEight);
+			this.Controls.Add(this.NumberNine);
+			this.Controls.Add(this.NumberZero);
+			this.Controls.Add(this.NumberFour);
+			this.Controls.Add(this.NumberFive);
+			this.Controls.Add(this.NumberSix);
+			this.Controls.Add(this.NumberThree);
+			this.Controls.Add(this.NumberTwo);
+			this.Controls.Add(this.NumberOne);
+			this.Name = "TextForm";
+			this.Text = "TextForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button NumberZero;
+		private System.Windows.Forms.Button NumberOne;
+		private System.Windows.Forms.Button NumberTwo;
+		private System.Windows.Forms.Button NumberThree;
+		private System.Windows.Forms.Button NumberFour;
+		private System.Windows.Forms.Button NumberFive;
+		private System.Windows.Forms.Button NumberSix;
+		private System.Windows.Forms.Button NumberSeven;
+		private System.Windows.Forms.Button NumberEight;
+		private System.Windows.Forms.Button NumberNine;
+		private System.Windows.Forms.Button AddBotton;
+		private System.Windows.Forms.Button MulButton;
+		private System.Windows.Forms.Button ExceptButton;
+		private System.Windows.Forms.Button LessBotton;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+	}
+}
